26f0f02830bd577504470ea52754a9871b027fe5
Sash249 <sashasanthosh10@gmail.com>
2026-02-26 16:19:04 +0530

    UC2:Hardcoded palindrome

--- src/palindromechecker.java
+++ src/palindromechecker.java
@@ -1,11 +1,21 @@
 public class palindromechecker {
 
-    static void main(String[]args){
-        System.out.println("========================================================");
-        System.out.println("Welcome to Palindrome Checker System");
-        System.out.println("========================================================");
-        System.out.println("Application version 1.0");
-        System.out.println();
-        System.out.println("System initialized successfully");
+    static void main(String[] args) {
+        String input = "madam";
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input String:" + input);
+        if (isPalindrome) {
+            System.out.println("Result:The string is a palindrome.");
+        } else {
+            System.out.println("Result:The string is NOT a palindrome.");
+        }
     }
 }
+
+
