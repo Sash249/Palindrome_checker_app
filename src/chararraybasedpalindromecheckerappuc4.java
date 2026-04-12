99a0e92f8a186a3ba82231d642ffbee866f9341c
Sash249 <sashasanthosh10@gmail.com>
2026-04-12 22:34:33 +0530

    .\UC4: char array based palindrome

---
+++ src/chararraybasedpalindromecheckerappuc4.java
@@ -0,0 +1,34 @@
+public class chararraybasedpalindromecheckerappuc4 {
+    public static void main(String[] args) {
+
+        // Declare and initialize input
+        String input = "radar";
+
+        // Convert string to character array
+        char[] chars = input.toCharArray();
+
+        // Initialize pointers
+        int start = 0;
+        int end = chars.length - 1;
+
+        // Assume palindrome initially
+        boolean isPalindrome = true;
+
+        // Compare characters using two-pointer technique
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
